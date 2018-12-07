e449dea49ae131708b4750e563bb0ac82ab96ab1
thelittlewozniak <nathan.pire@gmail.com>
2018-12-07 15:45:05 +0100

    editing

--- JAVA/src/Serialize/PireJSON.java
+++ JAVA/src/Serialize/PireJSON.java
@@ -3,10 +3,11 @@ package Serialize;
 import Serialize.Serialize;
 
 public class PireJSON {
-    public static String Serialize(Object o){
+    public static String serialize(Object o){
         return Serialize.serialize(o);
     }
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.println("Emacs >VIM");
+
     }
 }
