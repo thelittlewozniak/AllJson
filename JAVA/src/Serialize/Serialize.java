f65880c38c659f867f14c6258fc5d5e21802959d
thelittlewozniak <nathan.pire@gmail.com>
2018-12-07 09:47:02 +0100

    handle String, Arrays of primitive and Strings

--- JAVA/src/Serialize/Serialize.java
+++ JAVA/src/Serialize/Serialize.java
@@ -1,6 +1,7 @@
 package Serialize;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 public class Serialize {
     public static String Serialize(Class serial){
@@ -15,6 +16,7 @@ public class Serialize {
         }
         for(int i = 0; i< fields.length; i++){
             var s= fields[i].getType();
+            System.out.println(s.toString());
             switch(s.toString()){
                 case "int":
                     try{
@@ -88,6 +90,181 @@ public class Serialize {
                         e.printStackTrace();
                     }
                     break;
+                case "class java.lang.String":
+                    try{
+                        fields[i].setAccessible(true);
+                        Object str=fields[i].get(t);
+                        string.append("\""+ fields[i].getName()+"\":\""+str+"\"");
+                    }
+                    catch (IllegalAccessException e){
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    if(fields[i].getType().isArray()){
+                        System.out.println(fields[i].getType().getComponentType());
+                        switch (fields[i].getType().getComponentType().toString()){
+                            case "int":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    int[] tab=(int[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append(tab[i]);
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case "long":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    long[] tab=(long[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append(tab[i]);
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case "short":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    short[] tab=(short[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append(tab[i]);
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case "byte":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    byte[] tab=(byte[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append(tab[i]);
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case "char":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    char[] tab=(char[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append(tab[i]);
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case "float":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    float[] tab=(float[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append(tab[i]);
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+
+                            case "double":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    double[] tab=(double[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append(tab[i]);
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+
+                            case "boolean":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    boolean[] tab=(boolean[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append(tab[i]);
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+
+                            case "class java.lang.String":
+                                try{
+                                    fields[i].setAccessible(true);
+                                    String[] tab=(String[])fields[i].get(t);
+                                    string.append("\""+fields[i].getName()+"\":[");
+                                    for(int j=0;j<tab.length;j++){
+                                        string.append("\""+tab[i]+"\"");
+                                        if(j<tab.length-1){
+                                            string.append(",");
+                                        }
+                                    }
+                                    string.append("]");
+                                }
+                                catch (IllegalAccessException e){
+                                    e.printStackTrace();
+                                }
+                                break;
+                        }
+                    }
+                    else if(fields[i].getType().equals(List.class)){
+                        string.append("list");
+                    }
             }
             if(i<fields.length-1){
                 string.append(",");
